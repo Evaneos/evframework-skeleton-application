444bc66f5e2b6b333bd8c1e9f6fbff3bb0489834
Olivier Madre <terenas@gmail.com>
2014-07-11 11:47:14 +0200

    default timezone is mandatory

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use EVFramework\Container\BaseBuilder as ContainerBuilder;
 use Pyrite\Routing\RouteCollectionBuilder as RouteCollectionBuilder;
 use Pyrite\Kernel\PyriteKernel as PyriteKernel;
 
+date_default_timezone_set('Europe/Paris');
+
 // We use the compiled version of the configuration
 // If you want to compile the configuration you should run compile.php
 // In dev env you can use grunt to automate this. Run grunt and a compiled version of the configuration will be created.
